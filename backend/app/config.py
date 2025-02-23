4affdccf60cb1d0e348fcbd55d27e0960fda2d75
Olaiwonismail <olaiwonismail@gmail.com>
2025-02-23 08:22:17 +0100

    dd

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,10 +4,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///sqlite.db'
     SECRET_KEY='b30bb8384e15acbc267f8cb0f1b94c84'
     MAIL_SERVER='smtp.gmail.com'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-    # MAIL_PORT=587
-    # MAIL_USE_TLS=True
-    # MAIL_USE_SSL=False
-    # MAIL_USERNAME='olaiwonismail@gmail.com'
-    # MAIL_PASSWORD='oladayo@14'
+   
